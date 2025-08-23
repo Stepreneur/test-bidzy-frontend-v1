d83e04676d9434e765d41ce0461d4fc29f850151
Stepreneur <kfranzaza13@gmail.com>
2025-08-23 23:23:22 +0700

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'localhost',
+      'res.cloudinary.com',     // สำหรับรูปภาพจาก Cloudinary
+      'profile.line-scdn.net',   // สำหรับรูปโปรไฟล์จาก LINE
+    ],
+  },
 };
 
 export default nextConfig;
